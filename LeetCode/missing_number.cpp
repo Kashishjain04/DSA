b308017fb5081a202d3c7596324c6d285fa4116f
Kashish Jain <jainabhishek7204@gmail.com>
2021-06-04 00:20:28 +0530

    Create missing_number.cpp

---
+++ LeetCode/missing_number.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0, n=nums.size();
+        for(auto i:nums) sum += i;
+        return n*(n+1)/2 - sum;
+    }
+};
