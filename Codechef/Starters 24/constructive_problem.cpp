78fa2451a3c635bbf39967dece8c74f363d305b7
KashishJain <55389276+Kashishjain04@users.noreply.github.com>
2022-02-03 09:57:46 +0530

    starters24

---
+++ Codechef/Starters 24/constructive_problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int         long long int
+#define mod         1000000007ll
+#define pinf        LLONG_MAX
+#define ninf        LLONG_MIN
+#define v(type)     vector<type>
+#define w(t)        int t;  cin >> t;   while (t--)
+#define f(i,x,y)    for (auto i=x; i<y; i++)
+#define FastIO      ios_base::sync_with_stdio(false);   cin.tie(NULL);  cout.tie(NULL);
+
+void file_i_o() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("/Users/kashish/Documents/C++/DSA/input.txt", "r", stdin);
+        freopen("/Users/kashish/Documents/C++/DSA/output.txt", "w", stdout);
+    #endif
+}
+
+void solve() {
+    int x; cin >> x;
+    int hsb = 31;
+    while (((1 << hsb) & x) == 0) hsb--;
+    hsb++;
+    cout << 0 << ' ' << x << ' ' << (1<<hsb | (1<<hsb)-1) << endl;
+}
+
+int32_t main() {
+    clock_t start = clock();
+    FastIO;
+    file_i_o();
+    w(t)
+        solve();
+
+    #ifndef ONLINE_JUDGE
+        clock_t end = clock();
+        cout << endl << endl << "Executed in: " << (double)(end - start) / double(CLOCKS_PER_SEC) << " sec";
+    #endif
+    return 0;
+}
